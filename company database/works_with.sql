5304a532a242ecdfed65e076a2ee466349007898
soubj <sobujahmed124@gmail.com>
2023-12-31 21:06:29 +0600

    created works_with table

---
+++ company database/works_with.sql
@@ -0,0 +1,10 @@
+CREATE TABLE works_with(
+    emp_id INT,
+    client_id INT,
+    total_sales INT,
+    PRIMARY KEY(emp_id, client_id),
+    FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
+    FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
+
+DESCRIBE works_with;
