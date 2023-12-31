104873434ac95799abbf16fdfc226ab2d5ca7426
soubj <sobujahmed124@gmail.com>
2023-12-31 21:00:40 +0600

    created client table

---
+++ company database/client.sql
@@ -0,0 +1,8 @@
+CREATE TABLE client(
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(30),
+    branch_id INT,
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE  SET NULL
+);
+
+describe client;
