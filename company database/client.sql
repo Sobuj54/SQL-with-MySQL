eeeeff92e2835968aca8f0921bfedc1a58e34686
soubj <sobujahmed124@gmail.com>
2023-12-31 21:48:44 +0600

    inserting data inside client and works_with table

--- company database/client.sql
+++ company database/client.sql
@@ -6,3 +6,12 @@ CREATE TABLE client(
 );
 
 describe client;
+
+-- CLIENT
+INSERT INTO client VALUES(400, 'Dunmore Highschool', 2);
+INSERT INTO client VALUES(401, 'Lackawana Country', 2);
+INSERT INTO client VALUES(402, 'FedEx', 3);
+INSERT INTO client VALUES(403, 'John Daly Law, LLC', 3);
+INSERT INTO client VALUES(404, 'Scranton Whitepages', 2);
+INSERT INTO client VALUES(405, 'Times Newspaper', 3);
+INSERT INTO client VALUES(406, 'FedEx', 2);
