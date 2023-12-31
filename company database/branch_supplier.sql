6ac93ac8b6d6e3d9eabe73b4f6881af5415c2725
soubj <sobujahmed124@gmail.com>
2023-12-31 21:44:36 +0600

    inserting data inside branch_supplier table

--- company database/branch_supplier.sql
+++ company database/branch_supplier.sql
@@ -12,3 +12,13 @@ DESCRIBE branch_supplier;
 INSERT INTO branch VALUES(1, "corporate", 100, '1254-10-12');
 
 SELECT * FROM branch;
+
+-- inserting data
+-- BRANCH SUPPLIER
+INSERT INTO branch_supplier VALUES(2, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
+INSERT INTO branch_supplier VALUES(2, 'J.T. Forms & Labels', 'Custom Forms');
+INSERT INTO branch_supplier VALUES(3, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier VALUES(3, 'Stamford Lables', 'Custom Forms');
